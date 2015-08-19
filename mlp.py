5b278682eb0c43c7db0d9a9b556db9f02816cb58
Peter Smit <peter.smit@aalto.fi>
2015-08-19 15:27:28 +0300

    Add random noise to input data

--- mlp.py
+++ mlp.py
@@ -7,6 +7,7 @@ import timeit
 
 import numpy
 
+from theano.tensor.shared_randomstreams import RandomStreams
 import theano.tensor as T
 import theano
 
@@ -314,7 +315,7 @@ class MLP(object):
     class).
     """
 
-    def __init__(self, rng, input, n_in, n_hidden, n_out):
+    def __init__(self, rng, input, n_in, n_hidden, n_out, noise_level):
         """Initialize the parameters for the multilayer perceptron
 
         :type rng: numpy.random.RandomState
@@ -341,9 +342,13 @@ class MLP(object):
         # into a HiddenLayer with a tanh activation function connected to the
         # LogisticRegression layer; the activation function can be replaced by
         # sigmoid or any other nonlinear function
+
+        self.srng = RandomStreams(seed=234)
+        noisy_input = input + noise_level * self.srng.normal(input.shape)
+
         self.hiddenLayer = HiddenLayer(
             rng=rng,
-            input=input,
+            input=noisy_input,
             n_in=n_in,
             n_out=n_hidden,
             activation=T.tanh
@@ -535,6 +540,8 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
     y = T.ivector('y')  # the labels are presented as 1D vector of
                         # [int] labels
 
+    noise_level = T.scalar('noise_level', dtype=theano.config.floatX)
+
     rng = numpy.random.RandomState(1234)
 
     # construct the MLP class
@@ -543,7 +550,8 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
         input=x,
         n_in=201,
         n_hidden=n_hidden,
-        n_out=61
+        n_out=61,
+        noise_level=noise_level,
     )
 
 
@@ -578,16 +586,20 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
         outputs=classifier.errors(y),
         givens={
             x: test_set_x[index * batch_size:(index + 1) * batch_size],
-            y: test_set_y[index * batch_size:(index + 1) * batch_size]
+            y: test_set_y[index * batch_size:(index + 1) * batch_size],
+            noise_level: 0.0,
         }
     )
+
+    theano.printing.pydotprint(test_model, outfile="test_model.png", var_with_name_simple=True)
 
     validate_model = theano.function(
         inputs=[index],
         outputs=classifier.errors(y),
         givens={
             x: valid_set_x[index * batch_size:(index + 1) * batch_size],
-            y: valid_set_y[index * batch_size:(index + 1) * batch_size]
+            y: valid_set_y[index * batch_size:(index + 1) * batch_size],
+            noise_level: 0.0,
         }
     )
 
@@ -611,16 +623,21 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
     # compiling a Theano function `train_model` that returns the cost, but
     # in the same time updates the parameter of the model based on the rules
     # defined in `updates`
+
+
     train_model = theano.function(
         inputs=[index],
         outputs=cost,
         updates=updates,
         givens={
             x: train_set_x[index * batch_size: (index + 1) * batch_size],
-            y: train_set_y[index * batch_size: (index + 1) * batch_size]
+            y: train_set_y[index * batch_size: (index + 1) * batch_size],
+            noise_level: 0.1,
         }
     )
     # end-snippet-5
+
+    theano.printing.pydotprint(train_model, outfile="train_model.png", var_with_name_simple=True)
 
     ###############
     # TRAIN MODEL #
